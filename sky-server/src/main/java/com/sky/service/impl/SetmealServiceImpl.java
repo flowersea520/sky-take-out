22c50da58494047add89ecb73f565fbd0191b9e6
mortal <2894266590@qq.com>
2024-01-07 01:31:49 +0800

    套餐分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,5 +64,26 @@ public class SetmealServiceImpl implements SetmealService {
 		// setmealDishes是前端传过来的，在DTO中，所以要保存到对应的关联表中去
 		setmealDishMapper.insertBatch(setmealDishes);
 
+	}
+
+	/**
+	 * 套餐分页查询
+	 * @param setmealPageQueryDTO
+	 * @return
+	 */
+	@Override
+	public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+		// 拿到前端传过来的页码和每页记录数
+		int pageNum = setmealPageQueryDTO.getPage(); // 页码
+		int pageSize = setmealPageQueryDTO.getPageSize(); // 每页记录数
+
+		// 使用分页插件，开始分页
+		// 当你调用这个方法后，后续的查询会自动应用分页逻辑，返回特定页的数据。
+		PageHelper.startPage(pageNum,pageSize);
+
+		// 调用mapper层（传入的肯定是实体类，这里用DTO三个参数，够了：可以根据需要，按照套餐名称、分类、售卖状态进行查询）
+		Page<SetmealVO> page =  setmealMapper.pageQuery(setmealPageQueryDTO);
+
+		return new PageResult(page.getTotal(), page.getResult()); // 这里对应接口文档的data
 	}
 }
